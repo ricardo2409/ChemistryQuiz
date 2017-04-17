2f8c4edd3acffdacecd57fd448ed3d958ad569ca
Ricardo <ricardotrevino24@gmail.com>
2017-04-16 19:00:10 -0500

    Adds images validation

---
+++ Glucosa/GameManager.swift
@@ -0,0 +1,19 @@
+//
+//  GameManager.swift
+//  Glucosa
+//
+//  Created by Ricardo on 4/16/17.
+//  Copyright © 2017 Ricardo. All rights reserved.
+//
+
+import Foundation
+
+final class GameManager {
+    
+    private init() { }
+    
+    static let sharedInstance: GameManager = GameManager()
+    
+    var counter = 0
+    
+}
--- Glucosa/JuegoViewController.swift
+++ Glucosa/JuegoViewController.swift
@@ -8,120 +8,138 @@
 
 import UIKit
 import DIOCollectionView
+import SCLAlertView
 
 
-class JuegoViewController: UIViewController, UICollectionViewDelegate, DIOCollectionViewDelegate, UICollectionViewDataSource, DIOCollectionViewDataSource {
+class JuegoViewController: UIViewController {
     
     @IBOutlet weak var collectionView1: DIOCollectionView!
     
-    var items1 = [("Celulosa", UIImage(named:"celulosa")!), ("Celobiosa", UIImage(named:"celobiosa")!), ("Glucosa", UIImage(named:"glucosa")!)]
+    @IBOutlet weak var imageView1: UIImageView!
+    @IBOutlet weak var imageView2: UIImageView!
+    @IBOutlet weak var imageView3: UIImageView!
+    
+    var imagenCelulosa:UIImage? = nil
+    var imagenCelobiosa:UIImage? = nil
+    var imagenGlucosa:UIImage? = nil
+    
+    var items1 = [(10, UIImage(named:"celulosa")!),
+                  (20, UIImage(named:"celobiosa")!),
+                  (30, UIImage(named:"glucosa")!)]
 
     
-    func itemsForCollectionView(collectionView: UICollectionView) -> [(String, UIImage)] {
-        print("itemsForCollectionView")
+    func itemsForCollectionView(collectionView: UICollectionView) -> [(Int, UIImage)] {
         if(collectionView == collectionView1) {
             return items1
         }
-       
-        
         return []
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("ViewDidLoad")
+        addObservers()
         collectionView1.delegate = self
         collectionView1.dataSource = self
         collectionView1.dioDataSource = self
         collectionView1.dioDelegate = self
         
-        
-        
-        // Do any additional setup after loading the view.
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print("ViewDidAppear")
-        navigationController?.navigationBar.isHidden = false
+        imagenCelulosa = UIImage(named:"celulosa")!
+        imagenCelobiosa = UIImage(named:"celobiosa")!
+        imagenGlucosa = UIImage(named:"glucosa")!
     }
     
-    
-    //DIOCollectionView Data Source
-    func dioCollectionView(_ dioCollectionView: DIOCollectionView, userDataForItemAtIndexPath indexPath: IndexPath) -> Any? {
-        print("userDataForItemAtIndexPath")
-
-        let cellData = itemsForCollectionView(collectionView: dioCollectionView)[indexPath.row]
-        
-        print(cellData)
-        
-        return cellData
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
     }
     
-    //DIOCollectionView Data Delegate
-    func dioCollectionView(_ dioCollectionView: DIOCollectionView, draggedItemAtIndexPath indexPath: IndexPath, withDragState dragState: DIODragState) {
-        print("draggedForItem")
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func reload(){
+        imageView1.image = UIImage()
+        imageView2.image = UIImage()
+        imageView3.image = nil
+    }
 
+}
+
+extension JuegoViewController: DIOCollectionViewDelegate {
+    func dioCollectionView(_ dioCollectionView: DIOCollectionView,
+                           draggedItemAtIndexPath indexPath: IndexPath,
+                           withDragState dragState: DIODragState) {
         switch(dragState) {
             
-//        case .began:
-//            self.items1.remove(at: indexPath.row)
-//            self.collectionView1.deleteItems(at: [indexPath])
+            //        case .began:
+            //            self.items1.remove(at: indexPath.row)
+            //            self.collectionView1.deleteItems(at: [indexPath])
             
         case .ended:
-            self.items1.remove(at: indexPath.row)
-            self.collectionView1.deleteItems(at: [indexPath])
-            //Checar si los 3 están correctos
+            items1.remove(at: indexPath.row)
+            collectionView1.deleteItems(at: [indexPath])
             
         default:
             break
         }
     }
-    
-    
-    //CollectionView Data Source
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        print("numberOfSections")
+}
 
+extension JuegoViewController: DIOCollectionViewDataSource {
+    func dioCollectionView(_ dioCollectionView: DIOCollectionView,
+                           userDataForItemAtIndexPath indexPath: IndexPath) -> Any? {
+        let cellData = itemsForCollectionView(collectionView: dioCollectionView)[indexPath.row]
+        return cellData
+    }
+}
+
+extension JuegoViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        print("cellForItem")
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-
+        
         let imageView  = cell.subviews[0].subviews[0] as! UIImageView
-        let label = cell.subviews[0].subviews[1] as! UILabel
+        //let label = cell.subviews[0].subviews[1] as! UILabel
         
         let item = itemsForCollectionView(collectionView: collectionView)[indexPath.row]
         print(item)
-        //let label as UILabel()!
         
         
-        label.text = "\(item.0)"
+        //label.text = "\(item.0)"
         imageView.image = item.1
         
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return itemsForCollectionView(collectionView: collectionView).count
-
+        
     }
-   
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension JuegoViewController: UICollectionViewDelegate {}
+
+private extension JuegoViewController {
+    func checkIfImagesInOrder() {
+        if(imageView1.tag == 10 && imageView2.tag == 20 || imageView2.tag == 20 && imageView3.tag == 30 || imageView1.tag == 10 && imageView3.tag == 30){
+            SCLAlertView().showSuccess("Correcto", subTitle: ":)")
+            print("correcto")
+            
+        } else {
+            SCLAlertView().showError("Incorrecto", subTitle: ":(")
+            print("incorrecto")
+            reload()
+            //                    self.collectionView1.reloadData()
+            
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func addObservers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.gameHasEnded), name: Notification.Name("gameHasEndedNotification"), object: nil)
     }
-    */
-
+    
+    @objc func gameHasEnded() {
+        checkIfImagesInOrder()
+    }
 }
